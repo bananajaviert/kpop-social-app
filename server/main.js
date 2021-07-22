6535aa0b797a7d8b39f9d3c43485445a5d67595f
Joe <=>
2021-07-22 17:37:35 +0800

    Initial Commit

---
+++ server/main.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import path from 'path'
+import url from 'url'
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
